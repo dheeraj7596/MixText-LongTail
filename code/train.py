03851653e7f30d37856c4d5b4341509876704c9e
Pushkar-Bhuse <pushkar.s.bhuse@gmail.com>
2022-04-30 00:55:16 -0700

    Temp Train changes

--- code/train.py
+++ code/train.py
@@ -87,8 +87,10 @@ parser.add_argument('--lambda-u-hinge', default=0, type=float,
                     help='weight for hinge loss term of unlabeled data')
 parser.add_argument('--nll_preprocessed', default=True, type=bool,
                     help='boolean to indicate if GPT-2 is used for calculating longtailedness')
-parser.add_argument('--long_tailed', default=False, type=bool,
+parser.add_argument('--long_tailed', default=True, type=bool,
                     help='boolean to indicate if Long-Tailedness should be computed')
+parser.add_argument('--new_method', default=True, type=bool,
+                    help='New Longtailedness score generation method')
 
 args = parser.parse_args()
 
@@ -106,6 +108,12 @@ print("Mix layers sets: ", args.mix_layers_set)
 
 
 def main():
+
+    print("Current Configuration:")
+    print("New Method: {}".format(args.new_method))
+    print("LongTailed: {}".format(args.long_tailed))
+
+
     global best_acc
     # Read dataset and build dataloaders
     train_labeled_set, train_unlabeled_set, val_set, test_set, n_labels = get_data(
@@ -115,7 +123,10 @@ def main():
     labeled_trainloader = Data.DataLoader(
         dataset=train_labeled_set, batch_size=args.batch_size, shuffle=True)
 
-    if args.long_tailed:
+    if args.new_method:
+        unlabeled_trainloader = None
+
+    elif args.long_tailed:
     
         if args.nll_preprocessed:
             sampler = get_nll_sampler(train_unlabeled_set.text)
@@ -161,8 +172,14 @@ def main():
     # Start training
     for epoch in range(args.epochs):
 
-        temp_data = train(labeled_trainloader, unlabeled_trainloader, model, optimizer,
-              scheduler, train_criterion, epoch, n_labels, args.train_aug)
+        if args.new_method:
+            unlabeled_trainloader = get_augmented_data(train_unlabeled_set, n_labels, model)
+
+            temp_data = nll_train(labeled_trainloader, unlabeled_trainloader, model, optimizer,
+                scheduler, train_criterion, epoch, n_labels, args.train_aug)
+        else:
+            temp_data = train(labeled_trainloader, unlabeled_trainloader, model, optimizer,
+                scheduler, train_criterion, epoch, n_labels, args.train_aug)
 
         logger = logger.append(temp_data, ignore_index=True)
 
@@ -219,8 +236,13 @@ def main():
 def get_nll_sampler(unlabelled):
     with open(args.data_path+'log_likelihood.pkl', 'rb') as f:
         nll_scores = np.array(pickle.load(f))
-    sampler = WeightedRandomSampler(nll_scores, len(unlabelled), replacement=True)
+    sampler = WeightedRandomSampler(nll_scores, len(unlabelled), replacement=False)
     return sampler
+
+def get_nll_scores():
+    with open(args.data_path+'log_likelihood.pkl', 'rb') as f:
+        nll_scores = np.array(pickle.load(f))
+    return nll_scores
 
 def get_tfidf_sampler(labelled, unlabelled):
     tfidfvectorizer = TfidfVectorizer(analyzer='word',stop_words= 'english')
@@ -232,6 +254,266 @@ def get_tfidf_sampler(labelled, unlabelled):
     sampler = WeightedRandomSampler(mean_vals, len(unlabelled), replacement=True)
 
     return sampler
+
+def get_tfidf_scores(labelled, unlabelled):
+    tfidfvectorizer = TfidfVectorizer(analyzer='word',stop_words= 'english')
+    tfidf_wm = tfidfvectorizer.fit_transform(labelled)
+
+    unlabelled_tfidf = tfidfvectorizer.transform(unlabelled)
+
+    mean_vals = softmax(np.negative(np.mean(unlabelled_tfidf.toarray(), axis = 1)))
+    return mean_vals
+
+def get_augmented_data(dataset, n_labels, model):
+
+    unlabeled_trainloader = Data.DataLoader(
+            dataset=dataset, 
+            batch_size=len(dataset.text), 
+            shuffle = False
+        )
+    
+    unlabelled_set = dataset
+    unlabeled_train_iter = iter(unlabeled_trainloader)
+
+    replicate = np.full((len(unlabelled_set)), n_labels, dtype=int)
+    (inputs_u, inputs_u2,  inputs_ori), (length_u,
+                                length_u2,  length_ori) = unlabeled_train_iter.next()
+
+    outputs_u = model(inputs_u)
+    outputs_u2 = model(inputs_u2)
+    outputs_ori = model(inputs_ori)
+
+    p = (0 * torch.softmax(outputs_u, dim=1) + 0 * torch.softmax(outputs_u2,
+                                                        dim=1) + 1 * torch.softmax(outputs_ori, dim=1)) / (1)
+
+    pt = p**(1/args.T)
+    targets_u = pt / pt.sum(dim=1, keepdim=True)
+    targets_u = targets_u.detach()
+    p_y = targets_u.numpy().flatten()
+
+    p_x_y = get_nll_scores()
+    p_x_y = np.repeat(p_x_y, replicate, axis=0)
+
+    unlabelled_set.text = np.repeat(unlabelled_set.text, replicate, axis=0)
+    unlabelled_set.ids = np.repeat(unlabelled_set.ids, replicate, axis=0)
+
+    scores = np.multiply(p_y, p_x_y)
+
+    sampler = WeightedRandomSampler(scores, len(scores), replacement=True)
+
+    temp_unlabelled = Data.DataLoader(
+                        dataset=unlabelled_set, 
+                        batch_size=args.batch_size_u, 
+                        sampler= sampler
+                    )
+    
+    unlabeled_loader = temp_unlabelled
+
+    return unlabeled_loader
+
+def nll_train(labeled_trainloader, unlabelled_trainloader, model, optimizer, scheduler, criterion, epoch, n_labels, train_aug=False):
+    labeled_train_iter = iter(labeled_trainloader)
+    unlabeled_train_iter = iter(unlabelled_trainloader)
+
+    model.train()
+
+    global total_steps
+    global flag
+    if flag == 0 and total_steps > args.temp_change:
+        print('Change T!')
+        args.T = 0.9
+        flag = 1
+    
+    temp_logger = pd.DataFrame({"label": []})
+
+    for batch_idx in range(args.val_iteration):
+
+        total_steps += 1
+
+        if not train_aug:
+            try:
+                inputs_x, targets_x, inputs_x_length = labeled_train_iter.next()
+            except:
+                labeled_train_iter = iter(labeled_trainloader)
+                inputs_x, targets_x, inputs_x_length = labeled_train_iter.next()
+        else:
+            try:
+                (inputs_x, inputs_x_aug), (targets_x, _), (inputs_x_length,
+                                                           inputs_x_length_aug) = labeled_train_iter.next()
+            except:
+                labeled_train_iter = iter(labeled_trainloader)
+                (inputs_x, inputs_x_aug), (targets_x, _), (inputs_x_length,
+                                                           inputs_x_length_aug) = labeled_train_iter.next()
+        try:
+            (inputs_u, inputs_u2,  inputs_ori), (length_u,
+                                                 length_u2,  length_ori) = unlabeled_train_iter.next()
+        except:
+            unlabeled_train_iter = iter(unlabelled_trainloader)
+            (inputs_u, inputs_u2, inputs_ori), (length_u,
+                                                length_u2, length_ori) = unlabeled_train_iter.next()
+
+        batch_size = inputs_x.size(0)
+        batch_size_2 = inputs_ori.size(0)
+        targets_x = torch.zeros(batch_size, n_labels).scatter_(
+            1, targets_x.view(-1, 1), 1)
+
+        if use_cuda:
+            inputs_x, targets_x = inputs_x.cuda(), targets_x.cuda(non_blocking=True)
+            inputs_u = inputs_u.cuda()
+            inputs_u2 = inputs_u2.cuda()
+            inputs_ori = inputs_ori.cuda()
+
+        mask = []
+
+        with torch.no_grad():
+            # Predict labels for unlabeled data.
+            outputs_u = model(inputs_u)
+            outputs_u2 = model(inputs_u2)
+            outputs_ori = model(inputs_ori)
+
+            # Based on translation qualities, choose different weights here.
+            # For AG News: German: 1, Russian: 0, ori: 1
+            # For DBPedia: German: 1, Russian: 1, ori: 1
+            # For IMDB: German: 0, Russian: 0, ori: 1
+            # For Yahoo Answers: German: 1, Russian: 0, ori: 1 / German: 0, Russian: 0, ori: 1
+            p = (0 * torch.softmax(outputs_u, dim=1) + 0 * torch.softmax(outputs_u2,
+                                                                         dim=1) + 1 * torch.softmax(outputs_ori, dim=1)) / (1)
+            # Do a sharpen here.
+            pt = p**(1/args.T)
+            targets_u = pt / pt.sum(dim=1, keepdim=True)
+            targets_u = targets_u.detach()
+
+        hard_targets = pd.DataFrame({"label" : torch.argmax(targets_u, dim=1).numpy()})
+        temp_logger = temp_logger.append(hard_targets, ignore_index=True)
+
+        mixed = 1
+
+        if args.co:
+            mix_ = np.random.choice([0, 1], 1)[0]
+        else:
+            mix_ = 1
+
+        if mix_ == 1:
+            l = np.random.beta(args.alpha, args.alpha)
+            if args.separate_mix:
+                l = l
+            else:
+                l = max(l, 1-l)
+        else:
+            l = 1
+
+        mix_layer = np.random.choice(args.mix_layers_set, 1)[0]
+        mix_layer = mix_layer - 1
+
+        if not train_aug:
+            all_inputs = torch.cat(
+                [inputs_x, inputs_u, inputs_u2, inputs_ori, inputs_ori], dim=0)
+
+            all_lengths = torch.cat(
+                [inputs_x_length, length_u, length_u2, length_ori, length_ori], dim=0)
+
+            all_targets = torch.cat(
+                [targets_x, targets_u, targets_u, targets_u, targets_u], dim=0)
+
+        else:
+            all_inputs = torch.cat(
+                [inputs_x, inputs_x_aug, inputs_u, inputs_u2, inputs_ori], dim=0)
+            all_lengths = torch.cat(
+                [inputs_x_length, inputs_x_length, length_u, length_u2, length_ori], dim=0)
+            all_targets = torch.cat(
+                [targets_x, targets_x, targets_u, targets_u, targets_u], dim=0)
+
+        if args.separate_mix:
+            idx1 = torch.randperm(batch_size)
+            idx2 = torch.randperm(all_inputs.size(0) - batch_size) + batch_size
+            idx = torch.cat([idx1, idx2], dim=0)
+
+        else:
+            idx1 = torch.randperm(all_inputs.size(0) - batch_size_2)
+            idx2 = torch.arange(batch_size_2) + \
+                all_inputs.size(0) - batch_size_2
+            idx = torch.cat([idx1, idx2], dim=0)
+
+        input_a, input_b = all_inputs, all_inputs[idx]
+        target_a, target_b = all_targets, all_targets[idx]
+        length_a, length_b = all_lengths, all_lengths[idx]
+
+        if args.mix_method == 0:
+            # Mix sentences' hidden representations
+            logits = model(input_a, input_b, l, mix_layer)
+            mixed_target = l * target_a + (1 - l) * target_b
+
+        elif args.mix_method == 1:
+            # Concat snippet of two training sentences, the snippets are selected based on l
+            # For example: "I lova you so much" and "He likes NLP" could be mixed as "He likes NLP so much".
+            # The corresponding labels are mixed with coefficient as well
+            mixed_input = []
+            if l != 1:
+                for i in range(input_a.size(0)):
+                    length1 = math.floor(int(length_a[i]) * l)
+                    idx1 = torch.randperm(int(length_a[i]) - length1 + 1)[0]
+                    length2 = math.ceil(int(length_b[i]) * (1-l))
+                    if length1 + length2 > 256:
+                        length2 = 256-length1 - 1
+                    idx2 = torch.randperm(int(length_b[i]) - length2 + 1)[0]
+                    try:
+                        mixed_input.append(
+                            torch.cat((input_a[i][idx1: idx1 + length1], torch.tensor([102]).cuda(), input_b[i][idx2:idx2 + length2], torch.tensor([0]*(256-1-length1-length2)).cuda()), dim=0).unsqueeze(0))
+                    except:
+                        print(256 - 1 - length1 - length2,
+                              idx2, length2, idx1, length1)
+
+                mixed_input = torch.cat(mixed_input, dim=0)
+
+            else:
+                mixed_input = input_a
+
+            logits = model(mixed_input)
+            mixed_target = l * target_a + (1 - l) * target_b
+
+        elif args.mix_method == 2:
+            # Concat two training sentences
+            # The corresponding labels are averaged
+            if l == 1:
+                mixed_input = []
+                for i in range(input_a.size(0)):
+                    mixed_input.append(
+                        torch.cat((input_a[i][:length_a[i]], torch.tensor([102]).cuda(), input_b[i][:length_b[i]], torch.tensor([0]*(512-1-int(length_a[i])-int(length_b[i]))).cuda()), dim=0).unsqueeze(0))
+
+                mixed_input = torch.cat(mixed_input, dim=0)
+                logits = model(mixed_input, sent_size=512)
+
+                #mixed_target = torch.clamp(target_a + target_b, max = 1)
+                mixed = 0
+                mixed_target = (target_a + target_b)/2
+            else:
+                mixed_input = input_a
+                mixed_target = target_a
+                logits = model(mixed_input, sent_size=256)
+                mixed = 1
+
+        Lx, Lu, w, Lu2, w2 = criterion(logits[:batch_size], mixed_target[:batch_size], logits[batch_size:-batch_size_2],
+                                       mixed_target[batch_size:-batch_size_2], logits[-batch_size_2:], epoch+batch_idx/args.val_iteration, mixed)
+
+        if mix_ == 1:
+            loss = Lx + w * Lu
+        else:
+            loss = Lx + w * Lu + w2 * Lu2
+
+        #max_grad_norm = 1.0
+        #torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # scheduler.step()
+
+        if batch_idx % 1000 == 0:
+            print("epoch {}, step {}, loss {}, Lx {}, Lu {}, Lu2 {}".format(
+                epoch, batch_idx, loss.item(), Lx.item(), Lu.item(), Lu2.item()))
+
+    return temp_logger
+
+
 
 def train(labeled_trainloader, unlabeled_trainloader, model, optimizer, scheduler, criterion, epoch, n_labels, train_aug=False):
     labeled_train_iter = iter(labeled_trainloader)
